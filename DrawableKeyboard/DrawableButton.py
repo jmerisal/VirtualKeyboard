3c6e96d20355711bc8db2d9897671b282c77e696
taavitaavi <taavitaavi@gmail.com>
2016-12-27 11:02:41 +0200

    added Drawable keyboard

--- DrawableKeyboard/DrawableButton.py
+++ DrawableKeyboard/DrawableButton.py
@@ -4,22 +4,25 @@ Created on Dec 26, 2016
 @author: taavi
 '''
 import cv2  # @UnresolvedImport
+#import numpy as np
 class DrawableButton():
     
-    def __init__(self,letter,size,font):
+    def __init__(self,letter,font):
         self.letter=letter
-        
-        self.size=size
         self.color=(200,255,155)
         self.font=font
         self.isSelected=False
         self.backgroundFill=1
-        self.pixelsPerCharacter=20
-        self.width=len(self.letter)*self.pixelsPerCharacter
-        self.height=self.pixelsPerCharacter*2
+        self.fontSize=0.7
+        self.pixelsPerCharacter=15
+        self.width=len(self.letter)*self.pixelsPerCharacter+self.pixelsPerCharacter
+        self.height=self.pixelsPerCharacter
     def drawButton(self,img,x=40,y=40):
-        cv2.rectangle(img, (x-self.pixelsPerCharacter, y+self.pixelsPerCharacter), (x+self.width+(self.pixelsPerCharacter), y-self.height), (0,0,0), thickness=self.backgroundFill) 
-        cv2.putText(img,self.letter,(x,y), self.font, 1, self.color, 3, cv2.LINE_AA)
+        height, width, channels = img.shape
+        
+        cv2.rectangle(img, (x-self.pixelsPerCharacter, y+self.pixelsPerCharacter), (x+round((self.width)*self.fontSize), y-self.height), (0,0,0), thickness=self.backgroundFill)
+         
+        cv2.putText(img=img,text=self.letter,org=(x,y), fontFace = self.font, fontScale=self.fontSize, color=self.color, thickness= 1,lineType= cv2.LINE_AA)
         
     def select(self):
         self.backgroundFill=cv2.FILLED
--- DrawableKeyboard/DrawableKeyboard.py
+++ DrawableKeyboard/DrawableKeyboard.py
@@ -6,11 +6,10 @@ Created on Dec 26, 2016
 import cv2  # @UnresolvedImport
 from DrawableButton import DrawableButton
 class DrawableKeyboard:
-    buttonSize=10
     def __init__(self):
-        self.buttonSize=10
-        self.font = cv2.FONT_HERSHEY_SIMPLEX
-        self.buttonGap=40
+     
+        self.font = cv2.FONT_HERSHEY_COMPLEX_SMALL
+        self.buttonGap=15
         
         self.alpha = {
             'row1' : ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p','/'],
@@ -26,23 +25,23 @@ class DrawableKeyboard:
         
         for k in self.alpha[row]:
             
-            button=DrawableButton(letter=k,size=self.buttonSize,font=self.font)
+            button=DrawableButton(letter=k,font=self.font)
             button.drawButton(img, x=buttonXCoordinate, y=buttonYCoordinate)
             buttonXCoordinate+=button.width+self.buttonGap
     
     def drawKeyboard(self,img):
-        #button=DrawableButton('[ space ]',10,self.font)
+        #button=DrawableButton('[ space ]',self.font)
         #button.drawButton(img)
         #row='row4'
         #self.drawRow(row=row, rowXCoordinate=40, rowYCoordinate=40, img=img)
         
         rowYCoordinate=40
-        
+         
         for row in self.alpha.keys():
-            print(row)
-        
+            #print(row)
+         
             self.drawRow(row=row, rowXCoordinate=40, rowYCoordinate=rowYCoordinate, img=img)
             rowYCoordinate+=40+self.buttonGap
-        
+         
         print("    ")     
         print("    ")       
